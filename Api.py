eeb13785542ab90879a9818e708009a8b8575093
dbrajesh007 <dbrajesh007@gmail.com>
2019-06-21 21:32:03 +0530

    1

--- Api.py
+++ Api.py
@@ -3,10 +3,25 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/price')
+@app.route('/a')
 def Hello():
-    return "hello_world"
+    return "hello_world1111"
     
+@app.route('/b')
+def Hello():
+    import pyodbc
+
+
+    server = 'sunthedb.database.windows.net'
+    database = 'sunthemart'
+    username = 'suntheadmin'
+    password = '$unthe123$'
+    
+    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ',1433', user=username, password=password, database=database)
+    cursor = conn.cursor()
+                             
+    return "hello_world2222"
+
 
 if __name__ == '__main__':
     app.run()
